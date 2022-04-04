8a3b30398ec49a1923da3b16cdd972cf8457e113
yuribertoli <yuribertoli@yahoo.it>
2022-04-04 19:15:58 +0200

    validazioni

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -18,7 +18,7 @@
 
             <div class="mb-3"> {{-- input tipo text --}}
               <label for="title" class="form-label">Titolo</label>
-              <input type="text" class="form-control" name="title" id="title">
+              <input type="text" class="form-control" name="title" id="title" value="{{old('title')}}">
             </div>
 
             <div class="mb-3"> {{-- inserisco una textarea per più righe text --}}
@@ -29,28 +29,27 @@
 
             <div class="mb-3"> {{-- input per inserire URL e validarla --}}
                 <label for="thumb" class="form-label">URL immagine da caricare</label>
-                <input type="url" class="form-control" name="thumb" id="thumb">
+                <input type="url" class="form-control" name="thumb" id="thumb" value="{{old('thumb')}}">
             </div>
 
             <div class="mb-3"> {{-- input per numeri, con step=".01" permetto fino a 2 decimali dopo la virgola --}}
               <label for="price" class="form-label">Prezzo</label>
-              <input type="number" class="form-control" step=".01" name="price" id="price">
+              <input type="number" class="form-control" step=".01" name="price" id="price" value="{{old('price')}}">
             </div>
 
             <select class="form-select mb-3" id="type" name="type"> {{-- select tag con options --}}
-                <option selected>Scegli tipo</option>
-                <option value="comic book">comic book</option>
-                <option value="graphic novel">graphic novel</option>
+                <option {{(old('type') == 'comic book') ? 'selected' : ''}} value="comic book">comic book</option>
+                <option {{(old('type') == 'graphic novel') ? 'selected' : ''}} value="graphic novel">graphic novel</option>
             </select>
 
             <div class="mb-3">
               <label for="series" class="form-label">Serie</label>
-              <input type="text" class="form-control" name="series" id="series">
+              <input type="text" class="form-control" name="series" id="series" value="{{old('series')}}">
             </div>
 
             <div class="mb-3"> {{-- input di tipo data (europea) --}}
               <label for="sale_date" class="form-label">Data</label>
-              <input type="date" class="form-control" name="sale_date" id="sale_date">
+              <input type="date" class="form-control" name="sale_date" id="sale_date" value="{{old('sale_date')}}">
             </div>
            
             {{-- bottone per inviare tramite metodo post --}}
--- resources/views/comic/edit.blade.php
+++ resources/views/comic/edit.blade.php
@@ -19,39 +19,39 @@
 
             <div class="mb-3"> {{-- input tipo text --}}
               <label for="title" class="form-label">Titolo</label>
-              <input type="text" class="form-control" name="title" id="title" value="{{$comic->title}}">
+              <input type="text" class="form-control" name="title" id="title" value="{{old('title', $comic->title)}}">
             </div>
 
             <div class="mb-3"> {{-- inserisco una textarea per più righe text --}}
               <label for="description" class="form-label">Descrizione</label>
               <textarea class="form-control" name="description" id="description">
-                {{$comic->description}}
+                {{old('description', $comic->description)}}
               </textarea>
             </div>
 
             <div class="mb-3"> {{-- input per inserire URL e validarla --}}
                 <label for="thumb" class="form-label">URL immagine da caricare</label>
-                <input type="url" class="form-control" name="thumb" id="thumb" value="{{$comic->thumb}}">
+                <input type="url" class="form-control" name="thumb" id="thumb" value="{{old('thumb', $comic->thumb)}}">
             </div>
 
             <div class="mb-3"> {{-- input per numeri, con step=".01" permetto fino a 2 decimali dopo la virgola --}}
               <label for="price" class="form-label">Prezzo</label>
-              <input type="number" class="form-control" step=".01" name="price" id="price" value="{{$comic->price}}">
+              <input type="number" class="form-control" step=".01" name="price" id="price" value="{{old('price', $comic->price)}}">
             </div>
 
             <select class="form-select mb-3" id="type" name="type"> {{-- select tag con options --}}
-                <option {{($comic->type == 'comic book') ? 'selected' : ''}} value="comic book">comic book</option>
-                <option {{($comic->type == 'graphic novel') ? 'selected' : ''}} value="graphic novel">graphic novel</option>
+                <option {{ (old('type', $comic->type) == 'comic book') ? 'selected' : '' }} value="comic book">comic book</option>
+                <option {{ (old('type', $comic->type) == 'graphic novel') ? 'selected' : '' }} value="graphic novel">graphic novel</option>
             </select>
 
             <div class="mb-3">
               <label for="series" class="form-label">Serie</label>
-              <input type="text" class="form-control" name="series" id="series" value="{{$comic->series}}">
+              <input type="text" class="form-control" name="series" id="series" value="{{old('series', $comic->series)}}">
             </div>
 
             <div class="mb-3"> {{-- input di tipo data (europea) --}}
               <label for="sale_date" class="form-label">Data</label>
-              <input type="date" class="form-control" name="sale_date" id="sale_date" value="{{$comic->sale_date}}">
+              <input type="date" class="form-control" name="sale_date" id="sale_date" value="{{old('sale_date', $comic->sale_date)}}">
             </div>
            
             {{-- bottone per inviare tramite metodo post --}}
